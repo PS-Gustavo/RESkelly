b2ecd15bf44b7d7d0c14c6ad30f9716a5ec572c2
Gustavo Resende Lopes <gustavolopesce17@gmail.com>
2023-02-26 11:07:31 -0300

    Added CMake support and initial build structure
    - Added CMake support and targets to compile and build dependencies
    - Validated current functionalities through the new build system
    
    Signed-off-by: Gustavo Resende Lopes <gustavolopesce17@gmail.com>

--- body/utils/skellyloader/skellyloader.cpp
+++ body/utils/skellyloader/skellyloader.cpp
@@ -3,7 +3,8 @@
 namespace skelly_loader {
 
     skellyloader::skellyloader() :
-        dllink("./output/skelly.so"),
+        dllink("./libskelly_dyn.so"),
+        //dllink("./output/skelly.so"),
         make_Application(load<application_t>("make_Application")) {
         if (make_Application == NULL) throw dynlib_error(std::string(dlerror()));
     }
