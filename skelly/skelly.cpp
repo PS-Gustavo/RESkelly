bd143e4fcb6d817fb93650f4c6972c95f7d8601e
Gustavo Resende Lopes <gustavolopesce17@gmail.com>
2023-03-27 10:29:03 -0300

    Updated header calls and prototype Event testing
    Signed-off-by: Gustavo Resende Lopes <gustavolopesce17@gmail.com>

--- skelly/skelly.cpp
+++ skelly/skelly.cpp
@@ -11,7 +11,7 @@ namespace skelly {
 
     void Application::run() {
         
-        skelly::logger::init();
+        skelly::Logger::init();
 
         char i = 64;
         while (i!=90) {
@@ -21,12 +21,21 @@ namespace skelly {
         }
 
         SKELLY_LOG_WARN("CHEESIUM");
+
+        WindowResizeEvent e(1280, 720);
+
+        std::cout << typeid(e).name() << "\n";
+        std::cout << "(" << e.getWidth() << ", " << e.getHeight() << ")\n";
+        
+        //SKELLY_LOG_TRACE(e);
+
+        //SKELLY_LOG_ERROR(e);
         
     }
 
     extern "C" {
-    std::unique_ptr<Application> make_Application() {
-        return std::make_unique<Application>();
-    }
+        std::unique_ptr<Application> makeApplication() {
+            return std::make_unique<Application>();
+        }
     }
 }
