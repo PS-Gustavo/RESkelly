2699aadcee3b6c303f1545631e378fd423d9bac2
Gustavo Resende Lopes <gustavolopesce17@gmail.com>
2023-04-03 12:23:07 -0300

    Added PCH(Precompiled Header) Functionality
    - Updated CMakeLists to reflect PCH functionality;
    - Updated headers to include libs only without PCH;
    - Updated included headers to reflect source code;
    - Updated pch to better work with Windows environments;
    
    Signed-off-by: Gustavo Resende Lopes <gustavolopesce17@gmail.com>

--- skelly/pch.h
+++ skelly/pch.h
@@ -1,21 +1,28 @@
 #pragma once
 
-#ifndef PCH_H
-#define PCH_H
+#ifdef PCH_ENABLED
+    #include <dlfcn.h>
 
-#include <dlfcn.h>
+    #include <iostream>
+    #include <sstream>
+    #include <utility>
+    #include <functional>
+    #include <memory>
+    #include <typeinfo>
 
-#include <iostream>
-#include <sstream>
-#include <utility>
-#include <functional>
-#include <memory>
-#include <typeinfo>
+    #include <stdexcept>
 
-#include <stdexcept>
+    #include <string>
+    #include <vector>
+    #include <map>
 
-#include <string>
-#include <vector>
-#include <map>
+    #include <spdlog/spdlog.h>
+    #include <spdlog/fmt/ostr.h>
+    #include <spdlog/sinks/stdout_color_sinks.h>
 
-#endif //PCH_H
+    #ifdef ENABLE_ASSERTS
+        #define SKELLY_ASSERT(x, ...) { if(!(x)) { SKELLY_LOG_ERROR("Assertion Failed: {0}", __VA_ARGS__); } }
+    #else
+        #define SKELLY_ASSERT(x, ...)
+    #endif
+#endif
