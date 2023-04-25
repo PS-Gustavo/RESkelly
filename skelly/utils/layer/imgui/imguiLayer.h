6ce325caec89f9ddffea9e3833d81526247c796a
Gustavo Resende Lopes <gustavolopesce17@gmail.com>
2023-04-25 02:52:17 -0300

    Added initial event handling for ImGui overlay
    Signed-off-by: Gustavo Resende Lopes <gustavolopesce17@gmail.com>

--- skelly/utils/layer/imgui/imguiLayer.h
+++ skelly/utils/layer/imgui/imguiLayer.h
@@ -5,6 +5,9 @@
 
 #ifndef PCH_ENABLED
     #include <layer.h>
+    #include <mouseEvent.h>
+    #include <keyEvent.h>
+    #include <windowEvent.h>
 #endif
 
 namespace skelly {
@@ -19,8 +22,25 @@ namespace skelly {
             void onUpdate();
             void onEvent(Event& event);
 
+            inline void setWindow(Window* window) { _m_window = window; }
+            inline Window* getWindow() { return _m_window; }
+
         private:
             float _m_time = 0.0f;
+            Window* _m_window = nullptr;
+
+            bool imguiResolveWindowClose();
+            bool imguiResolveWindowResize();
+            bool imguiResolveWindowFocus();
+            bool imguiResolveWindowLostFocus();
+            bool imguiResolveWindowMoved();
+            bool imguiResolveKeyPressed();
+            bool imguiResolveKeyReleased();
+            bool imguiResolveMouseButtonPressed(MouseButtonPressedEvent& event);
+            bool imguiResolveMouseButtonReleased(MouseButtonReleasedEvent& event);
+            bool imguiResolveMouseScrolled(MouseScrolledEvent& event);
+            bool imguiResolveMouseMoved(MouseMovedEvent& event);
+
     };
 
 }
