3f2d2eb5290cf9f0ea17a1bf30450ec8dea2fef5
Gustavo Resende Lopes <gustavolopesce17@gmail.com>
2023-07-12 22:03:21 -0300

    Disabled windowing feature of Imgui for OpenGL3
    - Feature has issues with Linux platform, and will be disabled until
    drag action is kept through window shifting and performance is
    increased

--- skelly/utils/layer/imgui/imguiLayer.cpp
+++ skelly/utils/layer/imgui/imguiLayer.cpp
@@ -24,7 +24,7 @@ namespace skelly {
         io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
         io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
         io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
-        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+        //io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
         //io.ConfigViewportsNoAutoMerge = true;
         //io.ConfigViewportsNoTaskBarIcon = true;
 
@@ -89,7 +89,7 @@ namespace skelly {
         static bool show = true;
 
         ImGui::ShowDemoWindow(&show);
-        
+
     }
 
     
