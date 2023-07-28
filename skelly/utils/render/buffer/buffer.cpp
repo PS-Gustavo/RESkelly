b88c0b16d1a671da30af49a388804fb001bab615
Gustavo Resende Lopes <gustavolopesce17@gmail.com>
2023-07-27 23:25:46 -0300

    Fixed Vertex Buffer target

--- skelly/utils/render/buffer/buffer.cpp
+++ skelly/utils/render/buffer/buffer.cpp
@@ -5,9 +5,11 @@ namespace skelly {
 
     VertexBuffer* VertexBuffer::create(float* vertices, uint32_t size) {
         switch (Renderer::getAPI()) {
-            
-            case RenderAPI::OpenGL:     return new OpenGLVertexBuffer(vertices, size);
-            default:                    SKELLY_ASSERT(false, "No Rendering API found!"); return nullptr;
+            case RenderAPI::OpenGL:
+                return new OpenGLVertexBuffer(vertices, size);
+            default:
+                SKELLY_ASSERT(false, "No Rendering API found!");
+                return nullptr;
         }
 
         return nullptr;
@@ -15,9 +17,11 @@ namespace skelly {
 
     IndexBuffer* IndexBuffer::create(uint32_t* indices, uint32_t size) {
         switch (Renderer::getAPI()) {
-
-            case RenderAPI::OpenGL:     return new OpenGLIndexBuffer(indices, size);
-            default:                    SKELLY_ASSERT(false, "No rendering API found!"); return nullptr;
+            case RenderAPI::OpenGL:
+                return new OpenGLIndexBuffer(indices, size);
+            default:
+                SKELLY_ASSERT(false, "No rendering API found!");
+                return nullptr;
         }
 
         return nullptr;
