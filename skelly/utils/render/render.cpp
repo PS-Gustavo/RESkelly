b55811feb1e664c83111645be51c316ebea3b32d
Gustavo Resende Lopes <gustavo@punti.com.br>
2023-09-06 02:26:22 -0300

    Expanded API support capability for Render Module
    - Updated CMakeList configs for consistency reasons;
    - Added issues WIP (to be fixes in pt. 2);
    - Minor lint;
    - Added extended API support for RendererAPI instantiation;
    	- Both in RendererAPI base APITarget member, as well
    	  as RendererCommand initialization routine;
    
    Signed-off-by: Gustavo Resende Lopes <gustavo@punti.com.br>

--- skelly/utils/render/render.cpp
+++ skelly/utils/render/render.cpp
@@ -1,3 +1,26 @@
+
+/****************************************************************************************
+ * 
+ * Zygomatic
+ * Render Subsystem
+ * 
+ ****************************************************************************************
+ * 
+ * Changelog:
+ * 
+ * - 0.1.0: Initial implementation; Vertex Array, Buffers and Shader basic functionality
+ * 
+ **************************************************************************************** 
+ * 
+ * Description:
+ * 
+ * This module provides access to higher level abstrations through the Renderer class,
+ * such as rendering a scene or drawing from a vertexArray index. It also provides the
+ * building blocks for the APIs through RendererCommands. RendererAPI is the abstraction
+ * to serve functionality from different rendering APIs.
+ * 
+ ***************************************************************************************/
+
 #include <render.h>
 #include <opengl.h>
 
@@ -6,11 +29,10 @@ namespace skelly {
     // Renderer
 
     void Renderer::beginScene() {
-
+        // orchestrate the matrices, environment, cameras, objects and shader info
     }
 
     void Renderer::endScene() {
-
     }
 
     void Renderer::submit(std::shared_ptr<VertexArray>& vertexArray) {
@@ -19,8 +41,19 @@ namespace skelly {
     }
 
     // Renderer Commands
+    
+    // Initializes interface according to Render GUI available
+    RendererAPI* RenderCommands::initialize() {
+        switch (Renderer::getTargetAPI()) {
+            case APITarget::OpenGL:
+                return new OpenGLRendererAPI();
+            default:
+                SKELLY_ASSERT(false, "RendererCommands::initialize: No Rendering API found!");
+                return nullptr;
+        }
 
-    RendererAPI* RenderCommands::_s_rendererAPI = new OpenGLRendererAPI;
-
-
+        SKELLY_ASSERT(false, "RendererCommands::initialize: unknown behavior.")
+        return nullptr;
+    }
+    RendererAPI* RenderCommands::_s_rendererAPI = RenderCommands::initialize();
 }
--- skelly/utils/render/rendererAPI.cpp
+++ skelly/utils/render/rendererAPI.cpp
@@ -1,7 +1,26 @@
+
+/****************************************************************************************
+ * 
+ * Zygomatic
+ * Render Subsystem
+ * 
+ ****************************************************************************************
+ * 
+ * Changelog:
+ * 
+ * - 0.1.0: Initial implementation; Vertex Array, Buffers and Shader basic functionality
+ * 
+ **************************************************************************************** 
+ * 
+ * Description:
+ * 
+ * This module provides access to higher level abstrations through the Renderer class,
+ * such as rendering a scene or drawing from a vertexArray index. It also provides the
+ * building blocks for the APIs through RendererCommands. RendererAPI is the abstraction
+ * to serve functionality from different rendering APIs.
+ * 
+ ***************************************************************************************/
+
 #include <rendererAPI.h>
 
-namespace skelly {
-    
-    APITarget RendererAPI::_s_targetAPI = APITarget::OpenGL;
-
-}
+namespace skelly {}
